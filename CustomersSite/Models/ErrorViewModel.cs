9b05ad7fe5a48d5d24349fb623131bb7ff644c43
LAPTOP-C90AH4P1\TGDD <nqumanh@gmail.com>
2022-10-10 12:37:03 +0700

    Initial Customers Site

---
+++ CustomersSite/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CustomersSite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
