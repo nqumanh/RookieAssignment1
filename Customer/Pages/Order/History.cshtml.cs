2ca8d6dd222bc5ec4eaff3ffcdb94e55dc50270b
LAPTOP-C90AH4P1\TGDD <nqumanh@gmail.com>
2022-11-07 14:44:44 +0700

    add pagination customer site

---
+++ Customer/Pages/Order/History.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http.Headers;
+using CustomerSite.Helper;
+using CustomerSite.Pages.Models;
+using System.Text;
+using SharedViewModels;
+
+namespace CustomerSite.Pages;
+
+public class OrderHistoryModel : PageModel
+{
+    private APIHelper _api = new APIHelper();
+
+    private readonly ILogger<OrderHistoryModel> _logger;
+
+    public OrderHistoryModel(ILogger<OrderHistoryModel> logger)
+    {
+        _logger = logger;
+    }
+    [BindProperty]
+    public List<OrderDTO> OrderHistory { get; set; } = null!;
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var UserId = HttpContext!.Request.Cookies["Id"]!;
+        HttpClient client = _api.initial();
+        var response = await client.GetAsync($"User/OrderHistory/{UserId}");
+        var result = response.Content.ReadAsStringAsync().Result;
+        OrderHistory = JsonConvert.DeserializeObject<List<OrderDTO>>(result) ?? new List<OrderDTO>();
+
+        return Page();
+    }
+}
+
