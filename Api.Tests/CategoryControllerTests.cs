51fa51945f52446d79658914a10c53356ea8a8c5
LAPTOP-C90AH4P1\TGDD <nqumanh@gmail.com>
2022-11-08 17:55:50 +0700

    unit test, higher coverage

--- Api.Tests/CategoryControllerTests.cs
+++ Api.Tests/CategoryControllerTests.cs
@@ -2,11 +2,11 @@ using Xunit;
 using Apis.Repository;
 using Apis.Models;
 using AutoMapper;
-using FakeItEasy;
 using Moq;
 using Apis.Controllers;
 using Apis.Profiles;
 using SharedViewModels;
+using System.Reflection;
 
 namespace Api.UnitTests;
 
@@ -23,7 +23,7 @@ public class CategoryControllerTests
     }
 
     [Fact]
-    public async Task Get_WhenSuccess_ReturnOk()
+    public async Task GetCatetegoryById_WhenSuccess_ReturnOk()
     {
         // Arrange
         var category = new Category
@@ -34,31 +34,31 @@ public class CategoryControllerTests
             Products = null,
         };
 
-        var returnCategoryDTO = new CategoryDTO
+        var expected = new CategoryDTO
         {
             Id = 1,
             Name = "Art",
             Description = "Books in the art nonfiction genre are about some sort of artistic form: painting, sculpting, etc.",
         };
 
-        int id = 1;
+        const int ID = 1;
 
         var categoryRepositoryMock = new Mock<ICategoryRepository>();
-        categoryRepositoryMock.Setup(s => s.Get(id)).Returns(Task.FromResult(category));
+        categoryRepositoryMock.Setup(s => s.Get(ID)).Returns(Task.FromResult<Category?>(category));
+        // var mapperMock = new Mock<IMapper>();
+        // mapperMock.Setup(s => s.Map<CategoryDTO>(category)).Returns(returnCategoryDTO);
 
-        var mapperMock = new Mock<IMapper>();
-        mapperMock.Setup(s => s.Map<CategoryDTO>(category)).Returns(returnCategoryDTO);
-
-        var controller = new CategoryController(categoryRepositoryMock.Object, mapperMock.Object);
+        var controller = new CategoryController(categoryRepositoryMock.Object, _mapper);
 
         // Act
-        var result = (await controller.Get(id));// as OkObjectResult;
+        var result = (await controller.Get(ID));
 
         // Assert
         Assert.NotNull(result);
-
-        var data = result.Value as CategoryDTO;
-        Assert.NotNull(data);
-        Assert.Equal(returnCategoryDTO, data);
+        IList<PropertyInfo> props = new List<PropertyInfo>(expected.GetType().GetProperties());
+        foreach (PropertyInfo prop in props)
+        {
+            Assert.Equal(prop.GetValue(expected), prop.GetValue(result.Value));
+        }
     }
 }
